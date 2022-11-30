9401976a0d52d8c49c77278ece5db0c576585b4b
daalti <daaltigo@gmail.com>
2022-11-30 18:13:08 +0100

    first structure

---
+++ js/app.js
@@ -0,0 +1,18 @@
+const header = document.querySelector("header");
+const sectionOne = document.querySelector(".home-intro");
+const btnMenu = document.querySelector(".header__btn-menu");
+const close = document.querySelector(".main-nav-close");
+const menu = document.querySelector(".main-nav");
+
+window.addEventListener("scroll", function () {
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+btnMenu.addEventListener("click", () => {
+  menu.classList.toggle("activo");
+  body.style.overflow = "hidden";
+});
+
+close.addEventListener("click", () => {
+  menu.classList.toggle("activo");
+});
